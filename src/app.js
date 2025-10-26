b9d4ab43e2db3f765f5c7d3091e91534859ad6ae
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-10-26 18:18:39 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -1,13 +1,39 @@
 const express = require("express");
 const app = express();
 
-app.use("/test",(req,res) => {
-    res.send("Hello from the server!");
-})
-
-app.use("/namaste",(req,res) => {
-    res.send("Namaste!")
-})
+app.use("/user",
+   [
+     (req,res,next) => 
+    {
+        console.log("Middleware 1 ran");
+        //res.send("Response-1");
+        next();
+    },
+    (req,res,next) => 
+    {
+        console.log("Middleware 2 ran");
+        //res.send("Response-2");
+        next();
+    },
+    (req,res,next) => 
+    {
+        console.log("Middleware 3 ran");
+        //res.send("Response-3");
+        next();
+    },
+    (req,res,next) => 
+    {
+        console.log("Middleware 4 ran");
+        //res.send("Response-4");
+        next();
+    },
+    (req,res,next) => 
+    {
+        console.log("Middleware 5 ran");
+        res.send("Response-5");
+    }
+   ]
+)
 
 app.listen(7777, () => {
     console.log("Server is successfully listening on port 7777.");
