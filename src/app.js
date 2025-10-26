a903b053955444985f5ca836560d68f1fa88619a
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-10-26 22:40:48 +0530

    middlewares and error handlers in express

--- src/app.js
+++ src/app.js
@@ -35,6 +35,54 @@ app.use("/user",
    ]
 )
 
+app.get("/example",
+    (req,res,next) => {
+    console.log("First Handler");
+    next();
+    },
+    (req,res) => {
+        res.send("Second Handler");
+    }
+)
+
+app.get('/skip', (req,res,next) => {
+    console.log("This handler will be skipped");
+    next('route');  // Skips to the next matching route handler
+},
+    (req,res) => {
+        res.send("You will not see this response because this handler is skipped");
+    }
+);
+
+app.get("/skip", (req,res) => {
+    res.send("Skipped to this route handler");
+})
+
+
+// Example of Middleware flow
+// Middleware 1: Request Logger
+app.use((req,res,next) => {
+    console.log(`${req.method} ${req.url}`);
+    next(); // Pass the control to next middleware
+});
+
+// Middleware 2: Authorization Check
+// Handle user authentcation for all admin routes using middlewares
+app.use("/admin",(req,res,next) => {
+    const token ="999";
+    const isAuthorizedAdmin = token === "999";
+    if(!isAuthorizedAdmin){
+        res.status(401).send("Unauthorized Admin");
+    }else {
+        next();
+    }
+})
+app.get("/admin/getAllData", (req,res) => {
+    res.send("All data generated")
+})
+app.get("/admin/deleteData", (req,res) => {
+    res.send("Data deleted");
+})
 app.listen(7777, () => {
     console.log("Server is successfully listening on port 7777.");
 });
