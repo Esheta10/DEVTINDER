d901e4df960290d9ed4a493165050163ce26d163
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-10-27 11:11:04 +0530

    DB CONNECTED

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 const express = require("express");
-require("./config/database")
+const connectDB = require("./config/database")
 const app = express();
 
 
-app.listen(7777, () => {
+connectDB()
+.then(() => {
+    console.log("Database connection established");
+    app.listen(7777, () => {
     console.log("Server is successfully listening on port 7777.");
-});
+    });
+})
+.catch(err => {
+    console.error("Database connection cannot be established");
+})
 
--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,8 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://eshetajaiswal99_db_user:Esheta@cluster0.wcwdnmr.mongodb.net/");
+    await mongoose.connect("mongodb+srv://eshetajaiswal99_db_user:Esheta@cluster0.wcwdnmr.mongodb.net/devTinder");
 }
 
-connectDB().then(() => {
-    console.log("Database connection established");
-}).catch(err => {
-    console.error("Database connection cannot be established");
-})
+module.exports = connectDB;
+
